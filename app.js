e6c16ef3470398f8b1879daf6f99c021ad266e2c
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-12-04 12:56:23 +0500

    Continue

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
              //      PostApp with Local Storage
 
+function register(){
+    event.preventDefault()
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+    var cpassword = document.getElementById("cpassword").value
+
+    var data = {
+        name,
+        email,
+        password,
+        cpassword
+    }
+}
